314fa4653049ff5158e5406b3dcb117c78a78864
ismilesky <fighter_kongfei@126.com>
2015-11-26 10:04:55 +0800

    Initial Commit

---
+++ ImagePickerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePickerDemo
+//
+//  Created by VS on 15/11/26.
+//  Copyright © 2015年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePickerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerDemo
+//
+//  Created by VS on 15/11/26.
+//  Copyright © 2015年 VS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
